7fe4fcdaea5c2080cb152ceccfa4fbf66d0adb76
sanggoalist <s.tran@goalist.co.jp>
2021-07-04 13:38:37 +0700

    Change to port 5000

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ mongoose.connection.once('open', (err, resp) => {
     }).catch(err => console.log(err));
 });
 
-app.listen(3000,()=>{
+app.listen(5000,()=>{
   console.log('đã bật server');
 })
 
